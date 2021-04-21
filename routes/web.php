e951ee3ed25fa9b58aeeda594d10079da9dee779
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-21 10:55:06 +0600

    access control allow origin problem solve

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,26 @@
 |
 */
 
-$router->get('/chart-data', 'ChartDataController@selectAllData');
-$router->get('/client-review', 'ClientReviewController@selectAllReview');
-$router->post('/add-contact-data', 'ContactController@addContactData');
+$router->get('/chart-data', ['middleware'=>'auth','uses'=>'ChartDataController@selectAllData']);
+$router->get('/client-review', ['middleware'=>'auth','uses'=>'ClientReviewController@selectAllReview']);
+$router->post('/add-contact-data', ['middleware'=>'auth','uses'=>'ContactController@addContactData']);
 
 // courses route here
-$router->get('/home-course', 'CourseController@selectFourseCourse');
-$router->get('/all-course', 'CourseController@selectAllCourse');
-$router->post('/course-details', 'CourseController@selectCourseDetails');
+$router->get('/home-course', ['middleware'=>'auth','uses'=>'CourseController@selectFourseCourse']);
+$router->get('/all-course', ['middleware'=>'auth','uses'=>'CourseController@selectAllCourse']);
+$router->post('/course-details', ['middleware'=>'auth','uses'=>'CourseController@selectCourseDetails']);
 
-$router->get('/footer-data', 'FooterController@selectFooterData');
-$router->get('/footer-information', 'FooterInformationController@selectFooterInformation');
-$router->get('/services-data', 'ServiceController@selectServicesData');
+$router->get('/footer-data', ['middleware'=>'auth','uses'=>'FooterController@selectFooterData']);
+$router->get('/footer-information', ['middleware'=>'auth','uses'=>'FooterInformationController@selectFooterInformation']);
+$router->get('/services-data', ['middleware'=>'auth','uses'=>'ServiceController@selectServicesData']);
 // Projects route here
-$router->get('/home-project', 'ProjectController@selectThreeCourse');
-$router->get('/all-projects', 'ProjectController@selectAllProjects');
-$router->post('/project-details', 'ProjectController@selectProjectDetails');
+$router->get('/home-project', ['middleware'=>'auth','uses'=>'ProjectController@selectThreeCourse']);
+$router->get('/all-projects', ['middleware'=>'auth','uses'=>'ProjectController@selectAllProjects']);
+$router->post('/project-details', ['middleware'=>'auth','uses'=>'ProjectController@selectProjectDetails']);
 
 //Home routes for HomeEtc table
-$router->get('/home-video', 'HomeEtcController@selectVideosData');
-$router->get('/project-client', 'HomeEtcController@selectTotalProjectClient');
-$router->get('/tech-description', 'HomeEtcController@selectTechDescription');
-$router->get('/title-subtitle', 'HomeEtcController@selectHomeTitleSubTitle');
+$router->get('/home-video', ['middleware'=>'auth','uses'=>'HomeEtcController@selectVideosData']);
+$router->get('/project-client', ['middleware'=>'auth','uses'=>'HomeEtcController@selectTotalProjectClient']);
+$router->get('/tech-description', ['middleware'=>'auth','uses'=>'HomeEtcController@selectTechDescription']);
+
+$router->get('/title-subtitle', ['middleware'=>'auth','uses'=>'HomeEtcController@selectHomeTitleSubTitle']);
